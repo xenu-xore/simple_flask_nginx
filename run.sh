f19149989f4ccd9eedb879aa5eb91285c4ae5482
xenu-xore <52954685+xenu-xore@users.noreply.github.com>
2021-02-08 17:54:56 +0300

    nginx

--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@
 docker build -t "flask_nginx" .
 docker run -d -p 5000:80 --name="flask_nginx" -v C:/Users/xor/PycharmProjects/flask_nginx1:/app "flask_nginx"
 
-# https://www.8host.com/blog/sborka-i-razvertyvanie-prilozheniya-flask-s-pomoshhyu-docker-v-ubuntu-18-04/
